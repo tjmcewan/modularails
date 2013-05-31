451b9c5162065e5ed9b8426ef6c7516613797b02
Tim McEwan <tim@mcewan.it>
2013-05-31 17:13:31 +1000

    pristine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Modularails::Application.config.secret_token = '6db84235e951299f8c3a98bc4fe24f7c52c4be6d88c8deab92e4e775f6330285329ddc577ec0367ae4334c5fe561bde0ba0c3f597c168112ca72cd8f15158a21'
