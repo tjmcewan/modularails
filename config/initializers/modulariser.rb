81beb910712cd191573ac9cf69543178e5a95a8f
Tim McEwan <tim@mcewan.it>
2013-06-02 20:44:03 +1000

    targeted module asset paths

--- config/initializers/modulariser.rb
+++ config/initializers/modulariser.rb
@@ -4,9 +4,5 @@ ActiveSupport.on_load(:action_controller) do
   view_paths << ActionView::FileSystemResolver.new("app", custom_pattern)
 end
 
-# setup assets for each non-rails folder under app/
-default_folders = ["assets", "controllers", "helpers", "mailers", "models", "views"]
-namespaces = Dir['app/*/'].map{ |a| File.basename(a) } - default_folders
-namespaces.each do |namespace|
-  Modularails::Application.config.assets.paths << Rails.root.join("app/#{namespace}/assets")
-end
+module_asset_paths = Dir["#{Rails.root}/app/**/"].select{ |dir| dir =~ /app\/.+\/assets/ }
+Modularails::Application.config.assets.paths.concat module_asset_paths
