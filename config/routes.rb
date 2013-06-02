2d6698cb314bb90bcd6a45939148db7f6462cff3
Tim McEwan <tim@mcewan.it>
2013-06-02 18:07:22 +1000

    set root route

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Modularails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'projects#index'
 
   # See how all your routes lay out with "rake routes"
 
